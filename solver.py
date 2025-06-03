7388d2e7324df6476fad513518874695cfb0cb99
Rahul S <shankarling_ra@student.kings.edu.au>
2025-06-04 09:32:47 +1000

    implemented is_valid_position function

--- solver.py
+++ solver.py
@@ -29,4 +29,30 @@ def print_board(board):
                 print(str(board[i][j]) + " ", end="") #includes space after printing and doesn't move to next line
                 
 
-print_board(sudokuboard)
+def is_valid_position(board, number, position):
+    #check ROW
+    #check each element in the row to see if there are any duplicates within the ROW 
+    for i in range(len(board[0])): 
+        if board[position[0]][i] == number: 
+            return False 
+        
+    #check COLUMN
+    #check each element in the column to see if there are any duplicates within the COLUMN
+    for i in range(len(board)):
+        if board[i][position[1]] == number: 
+            return False 
+        
+    #check BOX 
+    #check each element in the box to see if there are any duplicates within the BOX
+    box_x = position[0] // 3 
+    box_y = position[1] // 3
+
+    for i in range(box_x * 3, box_x*3 + 3):
+        for j in range(box_y*3, box_y*3 + 3):
+            if board[i][j] == number and (i, j) != position:
+                return False 
+            
+    return True 
+
+
+
